a7e9577f481cc2949836c33bf39f3cce200918c6
adianitafresita <20220125@ricaldone.edu.sv>
2024-07-01 00:38:16 -0600

    Registro y login funcional, lo de cerrar sesion no estoy segura si funciona

--- SeaSmartMobile/src/components/Inputs/MaskedInputTelefono.js
+++ SeaSmartMobile/src/components/Inputs/MaskedInputTelefono.js
@@ -1,7 +1,18 @@
+// MaskedInputTelefono.js
 import { StyleSheet } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text';
 
 export default function MaskedInputTelefono({ telefono, setTelefono, placeholder }) {
+    const handleTelefonoChange = (text) => {
+        if (placeholder === "Teléfono fijo" && text.length === 1 && text !== '2') {
+            return;
+        }
+        if (placeholder === "Teléfono móvil" && text.length === 1 && text !== '7') {
+            return;
+        }
+        setTelefono(text);
+    };
+
     return (
         <TextInputMask
             style={styles.Input}
@@ -12,7 +23,7 @@ export default function MaskedInputTelefono({ telefono, setTelefono, placeholder
                 mask: '9999-9999' // Formato para el número de teléfono
             }}
             value={telefono}
-            onChangeText={setTelefono}
+            onChangeText={handleTelefonoChange}
         />
     );
 }
@@ -27,5 +38,4 @@ const styles = StyleSheet.create({
         padding: 5,
         marginVertical: 10
     },
-
 });
--- SeaSmartMobile/src/screens/Registro.js
+++ SeaSmartMobile/src/screens/Registro.js
@@ -1,82 +1,38 @@
-
-import { StyleSheet, Text, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+// Registro.js
+import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native';
 import { useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import * as Constantes from '../utils/Constantes'
+import * as Constantes from '../utils/Constantes';
 import Constants from 'expo-constants';
-//Import de componentes
-import Input from '../components/Inputs/Input'
-import InputMultiline from '../components/Inputs/InputMultiline'
-import Buttons from '../components/Buttons/Buttons';
+// Importar componentes
+import Input from '../components/Inputs/Input';
+import InputEmail from '../components/Inputs/InputEmail';
 import MaskedInputTelefono from '../components/Inputs/MaskedInputTelefono';
 import MaskedInputDui from '../components/Inputs/MaskedInputDui';
-import InputEmail from '../components/Inputs/InputEmail';
+import Buttons from '../components/Buttons/Buttons';
 
-
-export default function Registro({ navigation }) {
+export default function SignUp({ navigation }) {
     const ip = Constantes.IP;
 
-    const [show, setShow] = useState(false);
-
-    const [nombre, setNombre] = useState('')
-    const [apellido, setApellido] = useState('')
-    const [email, setEmail] = useState('')
-    const [dui, setDui] = useState('')
-    const [telefono, setTelefono] = useState('')
-    const [telefonoMovil, setTelefonoMovil] = useState('')
-    const [clave, setClave] = useState('')
-    const [confirmarClave, setConfirmarClave] = useState('')
-
-    // Expresiones regulares para validar DUI y teléfono
-    const duiRegex = /^\d{8}-\d$/;
-    const telefonoRegex = /^\d{4}-\d{4}$/;
-
-    const showMode = (currentMode) => {
-        setShow(true);
-        setMode(currentMode);
-    };
-
-    const showDatepicker = () => {
-        showMode('date');
-    };
-
-    /*
-        Fin Codigo para mostrar el datetimepicker
-        */
-
-    //props que recibe input
-    //placeHolder, setValor, contra, setTextChange
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [dui, setDui] = useState('');
+    const [telefono_fijo, setTelefono_Fijo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [contra, setContra] = useState('');
+    const [confirmarContra, setConfirmarContra] = useState('');
 
     const handleCreate = async () => {
         try {
-
-            // Validar los campos
-            if (!nombre.trim() || !apellido.trim() || !email.trim() ||
-                !dui.trim() || !telefonoMovil.trim() || !telefono.trim() || !clave.trim() || !confirmarClave.trim()) {
-                Alert.alert("Debes llenar todos los campos");
-                return;
-            } else if (!duiRegex.test(dui)) {
-                Alert.alert("El DUI debe tener el formato correcto (########-#)");
-                return;
-            } else if (!telefonoRegex.test(telefono)) {
-                Alert.alert("El teléfono debe tener el formato correcto (####-####)");
-                return;
-            } else if (!telefonoRegex.test(telefonoMovil)) {
-                Alert.alert("El teléfono debe tener el formato correcto (####-####)");
-                return;
-            }
-
-            // Si todos los campos son válidos, proceder con la creación del usuario
             const formData = new FormData();
             formData.append('nombreCliente', nombre);
             formData.append('apellidoCliente', apellido);
-            formData.append('correoCliente', email);
+            formData.append('correoCliente', correo);
             formData.append('duiCliente', dui);
-            formData.append('telefonoCliente', telefono);
-            formData.append('telefonoMovilCliente', telefonoMovil);
-            formData.append('claveCliente', clave);
-            formData.append('confirmarClave', confirmarClave);
+            formData.append('telefonoFijo', telefono_fijo); // nombre cambiado para coincidir con el PHP
+            formData.append('telefonoMovil', telefono); // nombre cambiado para coincidir con el PHP
+            formData.append('claveCliente', contra);
+            formData.append('confirmarClave', confirmarContra);
 
             const response = await fetch(`${ip}/SeaSmart/api/services/public/clientes.php?action=signUp`, {
                 method: 'POST',
@@ -91,62 +47,25 @@ export default function Registro({ navigation }) {
                 Alert.alert('Error', data.error);
             }
         } catch (error) {
-            console.log(error);
             Alert.alert('Ocurrió un error al intentar crear el usuario');
         }
     };
-
 
     return (
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollViewStyle}>
                 <Text style={styles.texto}>Registrar Usuario</Text>
-                <Input
-                    placeHolder='Nombre Cliente'
-                    setValor={nombre}
-                    setTextChange={setNombre}
-                />
-                <Input
-                    placeHolder='Apellido Cliente'
-                    setValor={apellido}
-                    setTextChange={setApellido}
-                />
-                <InputEmail
-                    placeHolder='Email Cliente'
-                    setValor={email}
-                    setTextChange={setEmail} />
-                <MaskedInputDui
-                    dui={dui}
-                    setDui={setDui} />
-
-                <MaskedInputTelefono
-                    telefono={telefono}
-                    placeholder={"Teléfono"}
-                    setTelefono={setTelefono} />
-
-                <MaskedInputTelefono
-                    telefonoMovil={telefonoMovil}
-                    placeholder={"Teléfono móvil"}
-                    setTelefono={setTelefonoMovil} />
-                <Input
-                    placeHolder='Clave'
-                    contra={true}
-                    setValor={clave}
-                    setTextChange={setClave} />
-                <Input
-                    placeHolder='Confirmar Clave'
-                    contra={true}
-                    setValor={confirmarClave}
-                    setTextChange={setConfirmarClave} />
-
-                <Buttons
-                    textoBoton='Registrar Usuario'
-                    accionBoton={handleCreate}
-                />
-
+                <Input placeHolder='Nombre Cliente' setValor={nombre} setTextChange={setNombre} />
+                <Input placeHolder='Apellido Cliente' setValor={apellido} setTextChange={setApellido} />
+                <InputEmail placeHolder='Email Cliente' setValor={correo} setTextChange={setCorreo} />
+                <MaskedInputDui dui={dui} setDui={setDui} />
+                <MaskedInputTelefono telefono={telefono_fijo} placeholder="Teléfono fijo" setTelefono={setTelefono_Fijo} />
+                <MaskedInputTelefono telefono={telefono} placeholder="Teléfono móvil" setTelefono={setTelefono} />
+                <Input placeHolder='Contraseña' contra={true} setValor={contra} setTextChange={setContra} />
+                <Input placeHolder='Confirmar contraseña' contra={true} setValor={confirmarContra} setTextChange={setConfirmarContra} />
+                <Buttons textoBoton='Registrar Usuario' accionBoton={handleCreate} />
             </ScrollView>
         </View>
-
     );
 }
 
@@ -154,21 +73,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#F7F5F4',
-        paddingTop: Constants.statusBarHeight + 5, // el 5 es para darle un pequeño margen cuando hay una camara en el centro de la pantalla
+        paddingTop: Constants.statusBarHeight + 5,
     },
     scrollViewStyle: {
         alignItems: 'center',
         justifyContent: 'center'
     },
     texto: {
-        color: '#322C2B', fontWeight: '900',
+        color: '#322C2B',
+        fontWeight: '900',
         fontSize: 20
     },
     textRegistrar: {
-        color: '#322C2B', fontWeight: '700',
+        color: '#322C2B',
+        fontWeight: '700',
         fontSize: 15
     },
-
     fecha: {
         fontWeight: '600',
         color: '#FFF'
@@ -180,11 +100,11 @@ const styles = StyleSheet.create({
     },
     contenedorFecha: {
         backgroundColor: '#A79277',
-        color: "#fff", fontWeight: '800',
+        color: "#fff",
+        fontWeight: '800',
         width: 250,
         borderRadius: 5,
         padding: 5,
         marginVertical: 10
     }
 });
-
